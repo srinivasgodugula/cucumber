cdff83cc686424bc1a526295696553bf252dabe0
personal <personal@personal-PC>
2018-05-01 14:58:39 +0530

    cucumberwithmaven

---
+++ FreeCrmBDDFramework/test-outout/report.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("C:/Users/personal/eclipse-workspace/cucumberwirhmavenfreecrm/src/main/java/feature/Deals.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 2,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "user enters username and password",
+  "rows": [
+    {
+      "cells": [
+        "naveenk",
+        "test@123"
+      ],
+      "line": 7
+    }
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user moves to new deals page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user enters deals details",
+  "rows": [
+    {
+      "cells": [
+        "firstdeal",
+        "",
+        "1000",
+        "50",
+        "30"
+      ],
+      "line": 12
+    }
+  ],
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Close the browser",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
